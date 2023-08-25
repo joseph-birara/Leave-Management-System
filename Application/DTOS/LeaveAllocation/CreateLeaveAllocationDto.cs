9af4a4fdee2375ccd4c03858a8c949b424cdcf02
joseph-birara <josephbirara92@gmail.com>
2023-08-25 20:54:56 +0300

    leave allocation Dtos added

---
+++ Application/DTOS/LeaveAllocation/CreateLeaveAllocationDto.cs
@@ -0,0 +1,13 @@
+using HR.LeaveManagement.Application.DTOs.Common;
+using HR.LeaveManagement.Application.DTOs.LeaveAllocation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HR.LeaveManagement.Application.DTOs.LeaveAllocation
+{
+    public class CreateLeaveAllocationDto
+    {
+        public int LeaveTypeId { get; set; }
+    }
+}
